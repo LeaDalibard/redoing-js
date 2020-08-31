03bad64c9b6f7975fa5f6bfce0a6cacc64343327
Lea Dalibard <leadalibard@gmail.com>
2020-08-31 15:08:10 +0200

    Ex 5 Spooky friday redone

--- js-complete-course/04-dates/05-get-spooky-fridays/script.js
+++ js-complete-course/04-dates/05-get-spooky-fridays/script.js
@@ -9,8 +9,18 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
-    // your code here
+(function () {
+    run.onclick = function (){
+        var year = document.getElementById("year").value;
+        var month = [];
+        var months=["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+        for (m = 0; m < 13; m++) {
+            var d = new Date(year,m, 13);
+            if (d.getDay() == 5) {
+                month.push(months[m])
+            }
+        }
+        alert(month);
+    }
 
 })();
